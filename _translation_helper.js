ba90265bd633835622f1c440d227b15e35474bc6
Wesley Mical <wesleymical@yahoo.com.br>
2025-11-29 20:20:52 -0300

    Add translation system to all pages

---
+++ _translation_helper.js
@@ -0,0 +1,37 @@
+// Helper script to add data-i18n attributes to content
+// This is a reference file showing which elements need translation attributes
+
+// For manual implementation or automated script
+
+const translationMapping = {
+    desenvolvedor: {
+        'Cloud Computing & AWS': 'developer.cloudTitle',
+        'Desenvolvedor Cloud AWS': 'developer.cloudRole',
+        'Principais Realizações:': 'common.achievements',
+        'Desenvolvimento de APIs': 'developer.apiTitle',
+        'Integração API REST': 'developer.apiRole',
+        'Processamento de Dados': 'developer.dataTitle',
+        'Manipulação de Dados': 'developer.dataRole',
+        'Stack Tecnológico': 'developer.stackTitle',
+        '← Voltar para Home': 'developer.backToHome'
+    },
+    consultor: {
+        'Implantação de Soluções Linx': 'consultant.implTitle',
+        'Consultor de Implantação': 'consultant.implRole',
+        'Soluções de Ponto de Venda': 'consultant.posTitle',
+        'Implantação Linx POS': 'consultant.posRole',
+        'Consultoria de Processos': 'consultant.processTitle',
+        'Otimização de Processos de Varejo': 'consultant.processRole',
+        'Portfólio de Clientes': 'consultant.portfolioTitle'
+    },
+    lsp: {
+        'Projeto H&M Brasil': 'lsp.hmTitle',
+        'Implantação H&M': 'lsp.hmRole',
+        'Piloto de Novos Produtos Linx': 'lsp.pilotTitle',
+        'Conciliador Equals para Linx ERP': 'lsp.equalsRole',
+        'SmartPOS para Linx POS': 'lsp.smartRole',
+        'Linx OmniPOS': 'lsp.omniRole',
+        'Consultoria LSP': 'lsp.consultTitle',
+        'Consultoria para Linx e Clientes': 'lsp.consultRole'
+    }
+};
